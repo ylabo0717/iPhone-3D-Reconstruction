3b5c670edfdd47a975f8ea3958164b3cb13bd86c
ylabo0717 <Yosuke.Horie@sony.com>
2021-08-17 23:59:33 +0900

    Add 3D Rotation

--- VoxelHashingGUI.bat
+++ VoxelHashingGUI.bat
@@ -1,4 +1,14 @@
 set DATA_PATH=%1
-.\bin\VoxelHashingGUI.exe %DATA_PATH% --intrinsics_json %DATA_PATH%\camera_intrinsic.json --device CPU:0
+@if "%2"=="" (
+    set DEVICE="CPU:0"
+) else (
+    set DEVICE=%2
+)
+
+set OUTPUT_PLY_FILENAME="scene.ply"
+set OUTPUT_LOG_FILENAME="trajectory.log"
+.\bin\VoxelHashingGUI.exe %DATA_PATH% --intrinsics_json %DATA_PATH%\camera_intrinsic.json --device %DEVICE%
 move scene.ply %DATA_PATH%
 move trajectory.log %DATA_PATH%
+move %OUTPUT_PLY_FILENAME% %DATA_PATH%
+move %OUTPUT_LOG_FILENAME% %DATA_PATH%
--- VoxelHashingGUI_cuda.bat
+++ VoxelHashingGUI_cuda.bat
@@ -1,4 +1,1 @@
-set DATA_PATH=%1
-.\bin\VoxelHashingGUI.exe %DATA_PATH% --intrinsics_json %DATA_PATH%\camera_intrinsic.json --device CUDA:0
-move scene.ply %DATA_PATH%
-move trajectory.log %DATA_PATH%
+call VoxelHashingGUI.bat %1 CUDA:0
