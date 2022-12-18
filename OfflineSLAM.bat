da36a00c13e67bfaebf79a38da90dba26a5981af
ylabo0717 <Yosuke.Horie@sony.com>
2022-12-18 20:27:15 +0900

    Update README.md

--- OfflineSLAM.bat
+++ OfflineSLAM.bat
@@ -9,6 +9,6 @@ set COLOR_DIR=%DATA_PATH%\color
 set DEPTH_DIR=%DATA_PATH%\depth
 set INTRINSIC_FILE_PATH=%DATA_PATH%\camera_intrinsic.json
 set OUTPUT_FILE_PATH=%DATA_PATH%\pointcloud.ply
-pushd .\bin\Release\
+pushd .\Release\
 .\OfflineSLAM.exe --color_folder_path %COLOR_DIR% --depth_folder_path %DEPTH_DIR% --intrinsic_path %INTRINSIC_FILE_PATH% --device %DEVICE% --pointcloud %OUTPUT_FILE_PATH% --vis
 popd
--- OnlineSLAM.bat
+++ OnlineSLAM.bat
@@ -7,6 +7,6 @@ set DATA_PATH=%~dp0%1
 
 set INTRINSIC_FILE_PATH=%DATA_PATH%\camera_intrinsic.json
 set OUTPUT_FILE_PATH=%DATA_PATH%\pointcloud.ply
-pushd .\bin\Release\
+pushd .\Release\
 .\OnlineSLAMRGBD.exe --dataset_path %DATA_PATH% --intrinsics_path %INTRINSIC_FILE_PATH% --device %DEVICE%
 popd
