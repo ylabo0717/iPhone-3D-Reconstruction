5a0faa702b2c7418f837ccf03cefae71188108f5
ylabo0717 <Yosuke.Horie@sony.com>
2023-11-22 22:31:09 +0900

    Fixed path to rotate_point_cloud.py

--- OfflineSLAM.bat
+++ OfflineSLAM.bat
@@ -11,4 +11,5 @@ set INTRINSIC_FILE_PATH=%DATA_PATH%\camera_intrinsic.json
 set OUTPUT_FILE_PATH=%DATA_PATH%\pointcloud.ply
 pushd .\Release\
 .\OfflineSLAM.exe --color_folder_path %COLOR_DIR% --depth_folder_path %DEPTH_DIR% --intrinsic_path %INTRINSIC_FILE_PATH% --device %DEVICE% --pointcloud %OUTPUT_FILE_PATH% --vis
+python ..\rotate_point_cloud.py .\scene.ply %DATA_PATH%\scene.ply 180 0 0
 popd
