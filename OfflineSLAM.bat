5d4e150bc7b39f9dd4a1b4fd1ce02ee7a8953464
ylabo0717 <Yosuke.Horie@sony.com>
2023-11-22 22:36:54 +0900

    Fixed path

--- OfflineSLAM.bat
+++ OfflineSLAM.bat
@@ -8,8 +8,8 @@ set DATA_PATH=%~dp0%1
 set COLOR_DIR=%DATA_PATH%\color
 set DEPTH_DIR=%DATA_PATH%\depth
 set INTRINSIC_FILE_PATH=%DATA_PATH%\camera_intrinsic.json
-set OUTPUT_FILE_PATH=%DATA_PATH%\pointcloud.ply
+set OUTPUT_FILE_PATH=%DATA_PATH%\scene.ply
 pushd .\Release\
-.\OfflineSLAM.exe --color_folder_path %COLOR_DIR% --depth_folder_path %DEPTH_DIR% --intrinsic_path %INTRINSIC_FILE_PATH% --device %DEVICE% --pointcloud %OUTPUT_FILE_PATH% --vis
-python ..\rotate_point_cloud.py .\scene.ply %DATA_PATH%\scene.ply 180 0 0
+.\OfflineSLAM.exe --color_folder_path %COLOR_DIR% --depth_folder_path %DEPTH_DIR% --intrinsic_path %INTRINSIC_FILE_PATH% --device %DEVICE% --pointcloud temp.ply --vis
+python ..\rotate_point_cloud.py .\temp.ply %OUTPUT_FILE_PATH% 180 0 0
 popd
