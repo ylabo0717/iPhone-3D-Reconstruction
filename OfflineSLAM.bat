50dccef6cbd318a6c273b194961bba7abd78f93f
ylabo0717 <Yosuke.Horie@sony.com>
2023-12-17 15:38:56 +0900

    Add command to delete old ply file

--- OfflineSLAM.bat
+++ OfflineSLAM.bat
@@ -10,6 +10,7 @@ set DEPTH_DIR=%DATA_PATH%\depth
 set INTRINSIC_FILE_PATH=%DATA_PATH%\camera_intrinsic.json
 set OUTPUT_FILE_PATH=%DATA_PATH%\scene.ply
 pushd .\open3d_bin\
+del .\temp.ply
 .\OfflineSLAM.exe --color_folder_path %COLOR_DIR% --depth_folder_path %DEPTH_DIR% --intrinsic_path %INTRINSIC_FILE_PATH% --device %DEVICE% --pointcloud temp.ply --vis
 python ..\rotate_point_cloud.py .\temp.ply %OUTPUT_FILE_PATH% 180 0 0
 popd
